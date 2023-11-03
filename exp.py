d7fa1b13454e4e70ec7f8bc95acc38066e2e85e8
ankit070923 <m23csa006@iitj.ac.in>
2023-11-03 20:55:44 +0530

    added docker file and related changes

--- exp.py
+++ exp.py
@@ -11,47 +11,84 @@ hand-written digits, from 0-9.
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
 # License: BSD 3 clause
 
-# Standard scientific Python imports# This example shows how scikit-learn can be used to recognize images of hand-written digits, from 0-9.
-
-
 # Import datasets, classifiers and performance metrics
-from utils import preprocess_data, train_model, split_train_dev_test,read_digits,predict_and_eval
+from sklearn import metrics, svm
 
-gama_ranges = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
-C_ranges = [0.1,1,2,5,10]
-# The digits dataset consists of 8x8 pixel images of digits. The images attribute of the dataset stores 8x8 arrays of grayscale values for each image. We will use these arrays to visualize the first 4 images. The target attribute of the dataset stores the digit each image represents and this is included in the title of the 4 plots below.
-# Note: if we were working from image files (e.g., ‘png’ files), we would load them using matplotlib.pyplot.imread.
+from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split, get_hyperparameter_combinations, tune_hparams
+from joblib import dump, load
+import pandas as pd
 
-# 1. Data Loading
+num_runs  = 1
+# 1. Get the dataset
+X, y = read_digits()
 
+# 2. Hyperparameter combinations
+classifier_param_dict = {}
+# 2.1. SVM
+gamma_list = [0.0001, 0.0005, 0.001, 0.01, 0.1, 1]
+C_list = [0.1, 1, 10, 100, 1000]
+h_params={}
+h_params['gamma'] = gamma_list
+h_params['C'] = C_list
+h_params_combinations = get_hyperparameter_combinations(h_params)
+classifier_param_dict['svm'] = h_params_combinations
 
-x,y = read_digits()
-
-# 3. Data splitting
-X_train, X_test,X_dev, y_train, y_test,y_dev = split_train_dev_test(x, y, test_size=0.3, dev_size=0.2);
-
-# 4. Data Preprocessing
-X_train = preprocess_data(X_train)
-X_test = preprocess_data(X_test)
-X_dev = preprocess_data(X_dev)
-
-best_accuracy_so_far = -1
-best_model = None
-for cur_gamma in gama_ranges:
-    for cur_c in C_ranges:
-        cur_model = train_model(X_train, y_train, {'gamma': cur_gamma,'C':cur_c}, model_type='svm')
-        cur_accuracy,predicted = predict_and_eval(cur_model, X_dev, y_dev)
-        if cur_accuracy > best_accuracy_so_far:
-            print(f"New best accuracy {cur_accuracy}")
-            best_accuracy_so_far = cur_accuracy
-            optimal_gamma = cur_gamma
-            optimal_C = cur_c
-            best_model = cur_model
-
-print(f"Optimal C is {optimal_C} , Optimal gamma is {optimal_gamma}")
+# 2.2 Decision Tree
+max_depth_list = [5, 10, 15, 20, 50, 100]
+h_params_tree = {}
+h_params_tree['max_depth'] = max_depth_list
+h_params_trees_combinations = get_hyperparameter_combinations(h_params_tree)
+classifier_param_dict['tree'] = h_params_trees_combinations
 
 
+results = []
+test_sizes =  [0.2]
+dev_sizes  =  [0.2]
+for cur_run_i in range(num_runs):
+    
+    for test_size in test_sizes:
+        for dev_size in dev_sizes:
+            train_size = 1- test_size - dev_size
+            # 3. Data splitting -- to create train and test sets                
+            X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
+            # 4. Data preprocessing
+            X_train = preprocess_data(X_train)
+            X_test = preprocess_data(X_test)
+            X_dev = preprocess_data(X_dev)
 
-# Predict the value of the digit on the test subset
-# 6.Predict and Evaluate 
-accuracy,predicted = predict_and_eval(best_model, X_test, y_test)
+            binary_preds = {}
+            model_preds = {}
+            for model_type in classifier_param_dict:
+                current_hparams = classifier_param_dict[model_type]
+                best_hparams, best_model_path, best_accuracy  = tune_hparams(X_train, y_train, X_dev, 
+                y_dev, current_hparams, model_type)        
+            
+                # loading of model         
+                best_model = load(best_model_path) 
+
+                test_acc, test_f1, predicted_y = predict_and_eval(best_model, X_test, y_test)
+                train_acc, train_f1, _ = predict_and_eval(best_model, X_train, y_train)
+                dev_acc = best_accuracy
+
+                print("{}\ttest_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}, test_f1={:.2f}".format(model_type, test_size, dev_size, train_size, train_acc, dev_acc, test_acc, test_f1))
+                cur_run_results = {'model_type': model_type, 'run_index': cur_run_i, 'train_acc' : train_acc, 'dev_acc': dev_acc, 'test_acc': test_acc}
+                results.append(cur_run_results)
+                binary_preds[model_type] = y_test == predicted_y
+                model_preds[model_type] = predicted_y
+                
+                print("{}-GroundTruth Confusion metrics".format(model_type))
+                print(metrics.confusion_matrix(y_test, predicted_y))
+
+
+print("svm-tree Confusion metrics".format())
+print(metrics.confusion_matrix(model_preds['svm'], model_preds['tree']))
+
+print("binarized predictions")
+print(metrics.confusion_matrix(binary_preds['svm'], binary_preds['tree'], labels=[True, False]))
+print("binarized predictions -- normalized over true labels")
+print(metrics.confusion_matrix(binary_preds['svm'], binary_preds['tree'], labels=[True, False] , normalize='true'))
+print("binarized predictions -- normalized over pred  labels")
+print(metrics.confusion_matrix(binary_preds['svm'], binary_preds['tree'], labels=[True, False] , normalize='pred'))
+        
+# print(pd.DataFrame(results).groupby('model_type').describe().T)
+                
